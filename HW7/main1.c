55b7ca1458c93cc84272e863f2498fb27239d822
B1129041 <119797293+nicole930405@users.noreply.github.com>
2022-12-29 01:29:02 +0800

    Add files via upload

---
+++ HW7/main1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n = 7, i = 0, j = 0;
+    char c = ' ';
+    for(i = 1; i <= n ; i++) {
+        printf("%*c", n-i+1, ' ');
+        for(j = 1; j <= i ; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
